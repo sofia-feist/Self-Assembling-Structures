932adac449254cc42f1b53a5fa6602c8b59d7d4e
Sofia Feist <SofiaFeist@users.noreply.github.com>
2020-07-02 11:47:26 +0100

    UDP Connection Unity/Grasshopper
    Added Unity/Grasshopper connection via UDP

---
+++ UDPReceive.cs
@@ -0,0 +1,104 @@
+﻿/*  
+    -----------------------
+    UDP-Receive
+    -----------------------
+    // Code adapted from: https://forum.unity.com/threads/simple-udp-implementation-send-read-via-mono-c.15900/
+   
+*/
+
+using System;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using UnityEngine;
+
+public class UDPReceive : MonoBehaviour
+{
+    // Receiving Thread
+    Thread receiveThread;
+ 
+    UdpClient client;
+    private int port = 8051;
+
+
+    // Received Message
+    private string receivedData = "";
+    private event EventHandler MessageReceived;
+
+
+
+
+    // Starts receiving thread when Unity starts
+    void Start()
+    {
+        // Create a new thread to read incoming UDP messages.
+        receiveThread = new Thread(new ThreadStart(ReceiveData));
+        receiveThread.IsBackground = true;
+        receiveThread.Start();
+
+        // Decodes the message when Message Received event is triggered
+        MessageReceived += new EventHandler((s, e) => DecodeMessage(s, e, receivedData));
+    }
+
+
+
+    // Closes the UDP connection and aborts the receiving thread when application is closed
+    void OnApplicationQuit()
+    {
+        if (receiveThread.IsAlive)
+            receiveThread.Abort();
+        
+        client.Close();
+    }
+
+
+
+    // Receive Data via UDP  
+    private void ReceiveData()
+    {
+        client = new UdpClient(port);
+
+        while (true)
+        {
+            try
+            {
+                // Bytes received
+                IPEndPoint anyIP = new IPEndPoint(IPAddress.Any, 0);
+                byte[] data = client.Receive(ref anyIP);
+
+                // Decode bytes into a string with the UTF8 encoding
+                string stringData = Encoding.UTF8.GetString(data);
+
+                // Update the received data
+                receivedData = stringData;
+                //print("Message Received: " + receivedData);
+
+                // Triggers the MessageReceived event to decode the message
+                OnMessageReceived(EventArgs.Empty);
+            }
+            catch (Exception err)
+            {
+                print(err.ToString());
+            }
+        }
+    }
+
+
+    // Trigger the MessageReceived event, which in turn triggers the DecodeMessage method
+    protected virtual void OnMessageReceived(EventArgs e)
+    {
+        MessageReceived?.Invoke(this, e);
+    }
+
+
+    // Decode the string Message received from Grasshopper (just coordinates for now)
+    private static void DecodeMessage(object sender, EventArgs e, string msg)
+    {
+        string[] data = msg.Split(',');
+            
+        Vector3 center = new Vector3(float.Parse(data[0]), float.Parse(data[1]), float.Parse(data[2]));
+
+        print(center);
+    }
+}
---
+++ UDPSend.cs
@@ -0,0 +1,81 @@
+﻿/*
+    -----------------------
+    UDP-Send
+    -----------------------
+    // Code adapted from: https://forum.unity.com/threads/simple-udp-implementation-send-read-via-mono-c.15900/
+
+*/
+
+using System;
+using System.Text;
+using System.Net.Sockets;
+using UnityEngine;
+
+public class UDPSend : MonoBehaviour
+{
+    // Connection properties
+    private string IP = "127.0.0.1";  // default localhost  
+    private int port = 8050;
+    UdpClient client;
+
+
+
+
+    // Connect to the remote host when Unity starts
+    void Start()
+    {
+        client = new UdpClient(IP, port);
+    }
+
+
+    // Closes the UDP connection when application is closed
+    void OnApplicationQuit()
+    {
+        client.Close();
+    }
+
+
+
+    // Encode Message to Send: includes the coordinates of the center of the cells and cell size
+    internal string EncodeMessage(Agent[] listAgents)
+    {
+        string msg = "";
+
+        foreach (Agent agent in listAgents)
+        {
+            Vector3 center = agent.Cell.Center;
+            string x = center.x.ToString();
+            string y = center.z.ToString();  // Swap y with z for Rhino Coordinates
+            string z = center.y.ToString();  //
+
+            string size = agent.Cell.CellSize.ToString();
+
+            // Construct Message
+            msg += $"{x},{y},{z},{size},";
+        }
+
+        msg = msg.Remove(msg.Length - 1);
+        return msg;
+    }
+
+
+    // Send Data via UDP
+    internal void SendData(string message)
+    {
+        try
+        {
+            // Print status
+            //print("Sending to " + IP + " : " + port);
+
+            // Encodes data in binary format using the UTF8 encoding
+            byte[] data = Encoding.UTF8.GetBytes(message);
+
+            // Sends the data to the remote host
+            client.Send(data, data.Length);
+        }
+        catch (Exception err)
+        {
+            print(err.ToString());
+        }
+    }
+}
